302e6cccfd74d567afb52e463c772211e57454e3
ibahmman <69468333+ibahmman@users.noreply.github.com>
2024-11-20 01:07:14 +0330

    Update mimikrotik.py

--- mimikrotik.py
+++ mimikrotik.py
@@ -7,14 +7,14 @@ COMMANDS = [
     '3 - Configure ipv6 settings.',
     '4 - Upgrade to the latest stable version.',
     '5 - Downgrade to the 6.49.12 stable version.',
-    '6 - Change IPv4.'
+
     '\n0 - Disconnect.'
 ]
 
-FTP_URL_OLDERVERSION_ROUTER = 'ftp://84.32.10.244/routeros-x86-6.49.12.npk'
-FTP_URL_LASTVERSION_ROUTER = 'ftp://84.32.10.244/routeros-lastest.npk'
-FTP_USER_LASTVERSION_ROUTER = 'ftp_mimik'
-FTP_PW_LASTVERSION_ROUTER = 'A@6461657464z'
+FTP_URL_OLDERVERSION_ROUTER = 'ftp://address-to-router-dir/routeros-x86-6.49.12.npk'
+FTP_URL_LASTVERSION_ROUTER = 'ftp://address-to-router-dir/routeros-lastest.npk'
+FTP_USER_LASTVERSION_ROUTER = 'ftp-user'
+FTP_PW_LASTVERSION_ROUTER = 'ftp-password'
 
 MESSAGE_WELCOME = """
   CCC      CCC       EEE                          EEEEEEEEEEE      UUU
@@ -35,37 +35,6 @@ MESSAGE_WELCOME = """
 
 
 # =========================================================================
-
-
-def generate_message(selection, **kwargs):
-    text = ''
-    match selection:
-        case 1:
-            text = f"""
-سرور مجازی شما از نو راه اندازی گردید سپس ورود به سرور انجام ، دسترس پذیری و کارکرد سرور بررسی شد.
-برای نگهداری امنیت سرور خود پیشنهاد میشود پس از ورود به سرور نسبت به تغییر پسورد اقدام نمایید.
-
-|آدرس|نام کاربر|گذرواژه|پورت وینباکس|پورت وب|
-| -------- | -------- | -------- | -------- | -------- |
-|{kwargs['host']}|admin|{kwargs['pw']}|443|505|
-"""
-        case 2:
-            text = f"""
-سرور مجازی شما از نو راه اندازی گردید سپس ورود به سرور انجام ، دسترس پذیری و کارکرد سرور بررسی شد.
-برای نگهداری امنیت سرور خود پیشنهاد میشود پس از ورود به سرور نسبت به تغییر پسورد اقدام نمایید.
-
-|آدرس|نام کاربر|گذرواژه|پورت وینباکس|پورت وب|
-| -------- | -------- | -------- | -------- | -------- |
-|{kwargs['host']}|admin|{kwargs['pw']}|{kwargs['winbox']}|{kwargs['www']}|
-"""
-        case 3:
-            text = f"""
-بر سرور مجازی شما آی پی ورژن 6 به شرح `{kwargs['ipv6']}` اختصاص یافت و پیکربندی های شبکه انجام گردید.
-با استفاده از لینک زیر امکان بررسی دسترس پذیری آی پی ورژن 6 و پینگ به آن بر شما میسر میباشد.
-`https://tools.keycdn.com/ipv6-ping`
-"""
-    pyperclip.copy(text)
-
     
 
 def generate_password():
@@ -111,7 +80,7 @@ api_port = None
 print(MESSAGE_WELCOME)
 
 while True:
-    host = input('Enter address: ').strip() # 87.248.155.63
+    host = input('Enter address: ').strip()
     password = input('Enter password: ').strip()
 
     try:
@@ -159,7 +128,7 @@ while True:
                     users.set(id='*1', password=pw)
                     conn.password = pw
 
-                    generate_message(selection, **{'host': host, 'pw': pw})
+                    
                     print(
                         'Mikrotic configured',
                         f'Username / Password: {conn.user}  {pw}',
@@ -185,7 +154,7 @@ while True:
                     users.set(id='*1', password=pw)
                     conn.password = pw
 
-                    generate_message(selection, **{'host': host, 'pw': pw, 'winbox': winbox, 'www': www})
+                    
                     print(
                         'Mikrotic configured',
                         f'Username / Password: {conn.user}  {pw}',
@@ -234,7 +203,7 @@ while True:
                         ipv6.add(address=_ipv6, interface=interface['id'])
                         route = api.get_resource('ipv6/route')
                         route.add(distance='1', gateway=_gw)
-                        generate_message(selection, **{'ipv6': _ipv6})
+                        
 
                         mk_ver = api.get_resource('system/resource').get()[0]['version']
                         if mk_ver.split('.')[0] == '7':
@@ -316,65 +285,4 @@ while True:
                                 break
                     
                 
-                case 64611657464:
-                    # change ipv4
-                    new_ipv4 = input('Enter a new IPv4: ')
-                    new_ipv4_subnetmask = input('Enter new subnetmask: ')
-                    new_ipv4_gw = input('Enter new gateway: ')
-
-                    if new_ipv4_subnetmask == '':
-                        new_ipv4_subnetmask = '24'
-
-                    if '/' in new_ipv4_subnetmask:
-                        new_ipv4_subnetmask = new_ipv4_subnetmask[1:]
-
-                    if new_ipv4_gw == '':
-                        octets = new_ipv4.split('.')
-                        octets[-1] = '1'
-                        new_ipv4_gw = '.'.join(octets)
-
-                    # create scheduler.
-                    # system scheduler add name="T2" start-time=startup interval=11s on-event="/ip address add address=111.11.111.11/24 interface=ether1"
-                    # system scheduler remove [find name="T2"]
-
-
-                    interface = [e for e in api.get_resource('interface').get() if e['type'] == 'ether' and e['disabled'] == 'false'][0]
-                    old_ipv4_index = [i['id'] for i in api.get_resource('ip/address').get() if i['address'][:-3] == conn.host][0]
-                    print('old_ipv4_index: ', old_ipv4_index)
-                    old_gw4_index = [i['id'] for i in api.get_resource('ip/route').get() if i['dst-address'] == '0.0.0.0/0' and i['distance'] == '1'][0]
-                    
-                    o = api.get_resource('ip/route').get()
-                    for _i in o:
-                        print(_i)
-
-                    # api.get_resource('system/scheduler').call('add', {
-                    #     'name': 'P_ADD_NEW_IPV4',
-                    #     'start-time': 'startup',
-                    #     'interval': '300s', # 5 min
-                    #     'on-event': f"/ip address add address={new_ipv4}/{new_ipv4_subnetmask} interface=interface['id']"
-                    # })
-                    # api.get_resource('system/scheduler').call('add', {
-                    #     'name': 'P_ADD_NEW_GW4',
-                    #     'start-time': 'startup',
-                    #     'interval': '300s', # 5 min
-                    #     'on-event': f"/ip route add gateway={new_ipv4_gw}"
-                    # })
-
-                    # api.get_resource('system/scheduler').call('add', {
-                    #     'name': 'P_DEL_OLD_IPV4',
-                    #     'start-time': 'startup',
-                    #     'interval': '360s', # 6 min
-                    #     'on-event': f"/ip address remove {old_ipv4_index}"
-                    # })
-                    # api.get_resource('system/scheduler').call('add', {
-                    #     'name': 'P_DEL_OLD_IPV4',
-                    #     'start-time': 'startup',
-                    #     'interval': '360s', # 6 min
-                    #     'on-event': f"/ip route remove {old_ipv4_index}"
-                    # })
-
-                    print('IP Address: ')
-                    s = api.get_resource('ip/address').get()
-                    for _i in s:
-                        print(_i)
-
+                
